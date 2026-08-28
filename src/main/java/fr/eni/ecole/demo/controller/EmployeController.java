43f324d689280f637e8e5132b56d5022adbf5013
Gregory <gmichaud@campus-eni.fr>
2026-08-28 14:27:56 +0200

    API POST

--- src/main/java/fr/eni/ecole/demo/controller/EmployeController.java
+++ src/main/java/fr/eni/ecole/demo/controller/EmployeController.java
@@ -5,10 +5,7 @@ import fr.eni.ecole.demo.bo.Employe;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -46,5 +43,16 @@ public class EmployeController {
         }
 
     }
+
+    @PostMapping
+    public ResponseEntity<?> ajoutEmploye(@RequestBody Employe employe){
+        try {
+            employeService.ajouter(employe);
+            return ResponseEntity.ok(employe);
+        }catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(e.getMessage());
+        }
+    }
+
 
 }
